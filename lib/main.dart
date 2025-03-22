940874fec9f3e16318274214af1beb1520aa69ca
likhon15-4915 <likhon15-4915@diu.edu.bd>
2025-03-23 00:30:22 +0600

    add drawer for primary visualization

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,58 @@ void main(){
     centerTitle: true,
     ),
 
+drawer: Drawer(
+  child: ListView(
+    children: [
+      DrawerHeader(child: Text("Drawer Header",
+        style: TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.normal,
+          color: Colors.white,
+        ),),
+        decoration: BoxDecoration(color: Colors.blueAccent),
+      ),
+      ListTile(
+        title: Text("Packages",),
+        onTap: (){
 
+        },
+      ),
+      ListTile(
+        title: Text("Tickets",),
+        onTap: (){
+
+        },
+      ),
+      ListTile(
+        title: Text("Hotel",),
+        onTap: (){
+
+        },
+      ),
+
+      ListTile(
+        title: Text("Accessories",),
+        onTap: (){
+
+        },
+      ),
+      ListTile(
+        title: Text("Navigation",),
+        onTap: (){
+
+        },),
+      ListTile(
+        title: Text("Emergemcies",),
+        onTap: (){
+
+        },
+      ),
+
+    ],
+
+  ),
+),
 
 
 
