58e714bdca2b32eea7aed498a3c92d2841709838
likhon15-4915 <likhon15-4915@diu.edu.bd>
2025-03-23 00:07:38 +0600

    Checking updates are stored or not

--- lib/main.dart
+++ lib/main.dart
@@ -1,2 +1,43 @@
 import 'package:flutter/material.dart';
 
+void main(){
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+      title: Text("Travel Pioneer",
+      style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Colors.red
+
+      ),
+      ),
+    backgroundColor: Colors.green,
+    centerTitle: true,
+    ),
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ),
+  ));
+
+}
