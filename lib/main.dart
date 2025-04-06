7b4dba57bd7f264f9cab21669f5ae0196137862f
hasanur4991 <hasanurmiah617@gmail.com>
2025-04-06 20:27:04 +0600

    Drawer created

--- lib/main.dart
+++ lib/main.dart
@@ -4,89 +4,76 @@ void main(){
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
-      title: Text("Travel Pioneer",
-      style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          color: Colors.red
+        title: Text("Travel Pioneer",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.red
 
-      ),
-      ),
-    backgroundColor: Colors.green,
-    centerTitle: true,
-    ),
-
-drawer: Drawer(
-  child: ListView(
-    children: [
-      DrawerHeader(child: Text("Drawer Header",
-        style: TextStyle(
-          fontSize: 15,
-          fontWeight: FontWeight.normal,
-          color: Colors.white,
-        ),),
-        decoration: BoxDecoration(color: Colors.blueAccent),
-      ),
-      ListTile(
-        title: Text("Packages",),
-        onTap: (){
-
-        },
-      ),
-      ListTile(
-        title: Text("Tickets",),
-        onTap: (){
-
-        },
-      ),
-      ListTile(
-        title: Text("Hotel",),
-        onTap: (){
-
-        },
+          ),
+        ),
+        backgroundColor: Colors.green,
+        centerTitle: true,
       ),
 
-      ListTile(
-        title: Text("Accessories",),
-        onTap: (){
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(child: Text("Drawer Header",
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.normal,
+                color: Colors.white,
+              ),),
+              decoration: BoxDecoration(color: Colors.blueAccent),
+            ),
+            ListTile(
+              leading: Icon(Icons.card_travel, size: 24, color: Colors.blue),
+              title: Text("Packages",),
+              onTap: (){
 
-        },
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.confirmation_number, size: 24, color: Colors.blue),
+              title: Text("Tickets",),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.hotel, size: 24, color: Colors.blue),
+              title: Text("Hotel",),
+              onTap: (){
+
+              },
+            ),
+
+            ListTile(
+              leading: Icon(Icons.backpack, size: 24, color: Colors.blue),
+              title: Text("Accessories",),
+              onTap: (){
+
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.navigation, size: 24, color: Colors.blue),
+              title: Text("Navigation",),
+              onTap: (){
+
+              },),
+            ListTile(
+              leading: Icon(Icons.local_hospital, size: 24, color: Colors.blue),
+              title: Text("Emergencies",),
+              onTap: (){
+
+              },
+            ),
+
+          ],
+
+        ),
       ),
-      ListTile(
-        title: Text("Navigation",),
-        onTap: (){
-
-        },),
-      ListTile(
-        title: Text("Emergemcies",),
-        onTap: (){
-
-        },
-      ),
-
-    ],
-
-  ),
-),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     ),
   ));
